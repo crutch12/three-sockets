1e6d1c094ed33e37a34ec5dae64397fc7ffb88b7
crutch12 <criitch@yandex.ru>
2017-10-31 10:06:53 +0300

    anim, shoots

--- public/js/game.js
+++ public/js/game.js
@@ -4,24 +4,53 @@ var renderer = new THREE.WebGLRenderer()
 renderer.setClearColor(0xAAA888)
 renderer.setSize(window.innerWidth, window.innerHeight)
 
+var loader = new THREE.JSONLoader();
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 
 camera.position.set(25, 25, 25)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
-var axes = new THREE.AxisHelper( 20 );
+var axes = new THREE.AxesHelper( 20 );
+axes.position.y = 1
 scene.add(axes);
 
-var planeGeometry = new THREE.PlaneGeometry(40, 40);
-var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
-var plane = new THREE.Mesh(planeGeometry,planeMaterial);
-plane.rotation.x=-0.5*Math.PI;
-scene.add(plane);
+// var planeGeometry = new THREE.PlaneGeometry(80, 80);
+// var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+// var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+// plane.rotation.x=-0.5*Math.PI;
+// scene.add(plane);
 
+var cubePlaneGeometry = new THREE.CubeGeometry(80, 1, 80)
+var cubePlaneMaterial = new THREE.MeshLambertMaterial({color: 0xBBBBBB});
+var cubePlane = new THREE.Mesh(cubePlaneGeometry, cubePlaneMaterial);
+cubePlane.position.y = -2
+scene.add(cubePlane)
 
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+var light = new THREE.HemisphereLight( 0xffffff, 0x003300, 1 );
+light.position.set(0, 5, 0 );
+scene.add( light );
+// var spotLight = new THREE.SpotLight( 0xffffff );
+// spotLight.position.set( 15, 35, 15 );
+// scene.add(spotLight );
 
 document.body.appendChild(renderer.domElement)
+
+var legs;
+var mixer;
+loader.load( './models/simple.js', function ( geometry, materials ) {
+	for (var k in materials) {
+		materials[k].skinning = true;
+	}
+	legs = new THREE.SkinnedMesh( geometry, materials );
+	legs.scale.set( 0.25, 0.25, 0.25 );
+	// scene.add( legs );
+	// mixer = new THREE.AnimationMixer( legs );
+	// mixer.clipAction( legs.geometry.animations[ 0 ] ).play();
+} );
+
 
 // var cubeGeometry = new THREE.CubeGeometry(1,1,1)
 // var cubeMaterial = new THREE.MeshLambertMaterial(
@@ -32,7 +61,7 @@ document.body.appendChild(renderer.domElement)
 
 var players = [];
 var player = null;
-
+var shoots = []
 // var socket = io(`${location.origin}:3000`);
 var socket = io();
 
@@ -82,6 +111,12 @@ socket.on('playerLeft', function(playerId) {
 	_.remove(players, {player: {id: playerId}})
 })
 
+socket.on('shootsMove', (shootsI) => {
+	for (var shoot of shootsI) {
+		var sh = _.find(shoots, {shoot: {id: shoot.id}})
+		sh.position.set(shoot.position.x, 0, shoot.position.z)
+	}
+})
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -92,9 +127,11 @@ function onWindowResize() {
 
 function render() {
 	requestAnimationFrame(render)
+	// if (mixer) mixer.update(0.015)
+
 	renderer.render(scene, camera)
 	for (var pl of players) {
-		pl.update(0.015 * 10)
+		pl.update(0.015)
 	}
 } 
 
--- public/js/player.js
+++ public/js/player.js
@@ -1,14 +1,3 @@
-// public static Vector3 MoveTowards(Vector3 current, Vector3 target, float maxDistanceDelta)
-// {
-// 	Vector3 a = target - current;
-// 	float magnitude = a.magnitude;
-// 	if (magnitude <= maxDistanceDelta || magnitude == 0f)
-// 	{
-// 	 return target;
-// 	}
-// 	return current + a / magnitude * maxDistanceDelta;
-// }
-
  function MoveTowards(current, target, maxDelta) {
 	 if (Math.abs(target - current) <= maxDelta)
 	 {
@@ -23,18 +12,29 @@
 
 class Player {
 	constructor(player, isMine) {
+		// console.log(legs)
 		this.player = player;
 		this.isMine = isMine
-		
-		var cubeGeometry = new THREE.CubeGeometry(1,1,1)
-		var cubeMaterial = new THREE.MeshBasicMaterial(
-			{color: new THREE.Color(`rgb(${player.color.r}, ${player.color.g}, ${player.color.b})`)})
-		var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-		cube.position.set(player.position.x, 1, player.position.z)
-		scene.add(cube)
+		this.isMoving = false
+		this.targetRotation = 0
+		// this.shoots = []
 
-		this.cube = cube
+		// var cubeGeometry = new THREE.CubeGeometry(1,1,1)
+		// var cubeMaterial = new THREE.MeshBasicMaterial(
+		// 	{color: new THREE.Color(`rgb(${player.color.r}, ${player.color.g}, ${player.color.b})`)})
+		// var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+		// cube.position.set(player.position.x, 1, player.position.z)
+		// scene.add(cube)
 
+		var mesh = legs.clone()
+		mesh = mesh
+
+		mesh.position.set(player.position.x, 0, player.position.z)
+		scene.add(mesh)
+
+		this.mesh = mesh
+
+		this.mixer = new THREE.AnimationMixer( this.mesh );
 		this.moving = {
 			x: 0,
 			z: 0
@@ -43,7 +43,7 @@ class Player {
 		if (this.isMine) {
 			console.log('mine', player.id)
 			window.addEventListener("keydown", event => {
-				var moving = _.clone(this.moving)
+			  var moving = _.clone(this.moving)
 			  switch (event.keyCode) {
 			    case 68: //d
 			      moving.x = 1;
@@ -56,6 +56,9 @@ class Player {
 			      break;
 			    case 87: //w
 			      moving.z = 1;
+			      break;
+			    case 32: //w
+			      this.shoot()
 			      break;
 			  }
 			  if (moving.x != this.moving.x || moving.z != this.moving.z) {
@@ -86,11 +89,26 @@ class Player {
 		socket.on('playerMove', (player) => {
 			if (this.player.id == player.id) {
 				// console.log(player.position)
-				// this.cube.position.set(player.position.x, 0, player.position.z)
+				// this.mesh.position.set(player.position.x, 0, player.position.z)
 				// this.moving = moving
 				this.player.position = player.position
 			}
 		})
+
+		socket.on('shoot', (shoot) => {
+			var cubeGeometry = new THREE.CubeGeometry(0.3,0.3,0.3)
+			var cubeMaterial = new THREE.MeshBasicMaterial(
+				{color: new THREE.Color(`rgb(${this.player.color.r}, ${this.player.color.g}, ${this.player.color.b})`)})
+			var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+			cube.shoot = shoot
+			cube.position.set(shoot.position.x, 0, shoot.position.z)
+			// cube.position.set(player.position.x, 1, player.position.z)
+			scene.add(cube)
+			shoots.push(cube)
+			console.log(shoots)
+		})
+
+
 	}
 
 	move() {
@@ -105,21 +123,46 @@ class Player {
 		socket.emit('move', this.moving)
 	}
 
-	update(delta) {
-		// this.cube.position.x = MoveTowards(this.cube.position.x, this.player.position.x, delta)
-		// this.cube.position.z = MoveTowards(this.cube.position.z, this.player.position.z, delta)
+	shoot() {
+		socket.emit('shoot', this.mesh.rotation.y)
+	}
 
-		
-		var diff = new THREE.Vector3().subVectors(this.cube.position, new THREE.Vector3(this.player.position.x, 1, this.player.position.z)).length()
-		if (diff > 5) {
-			this.cube.position.set(this.player.position.x, 1, this.player.position.z)
+	update(delta) {
+		// this.mesh.position.x = MoveTowards(this.mesh.position.x, this.player.position.x, delta)
+		// this.mesh.position.z = MoveTowards(this.mesh.position.z, this.player.position.z, delta)
+
+
+
+
+		var diffVec = new THREE.Vector3().subVectors(this.mesh.position, new THREE.Vector3(this.player.position.x, 0, this.player.position.z))
+		if (diffVec.length() > 5) {
+			this.mesh.position.set(this.player.position.x, 0, this.player.position.z)
 		} else {
-			this.cube.position.lerp( new THREE.Vector3(this.player.position.x, 1, this.player.position.z), delta )
+			this.mesh.position.lerp( new THREE.Vector3(this.player.position.x, 0, this.player.position.z), delta * 10)
+			
+			var quaternion = new THREE.Quaternion();
+			quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), this.targetRotation);
+			
+			this.mesh.quaternion.slerp(quaternion, delta * 10)
+			// this.mesh.rotation.y = MoveTowards(this.mesh.rotation.y, this.targetRotation, delta * 10)
 		}
-		console.log(diff)
-		// console.log(this.moving.x)
-		// this.cube.position.x -= this.moving.x * delta
-		// this.cube.position.z += this.moving.z * delta
+
+		if (this.mixer) this.mixer.update(delta)
+
+		if (diffVec.length() > 0.05) {
+			if (!this.isMoving) {
+				this.isMoving = true
+				this.mixer.clipAction( this.mesh.geometry.animations[ 0 ] ).play();
+			}
+
+			this.targetRotation = Math.atan2(diffVec.x, diffVec.z)
+			// this.mesh.rotation.y = 
+		} else {
+			if (this.isMoving) {
+				this.isMoving = false
+				this.mixer.clipAction( this.mesh.geometry.animations[ 0 ] ).stop();
+			}
+		}
 	}
 
 	leave() {
